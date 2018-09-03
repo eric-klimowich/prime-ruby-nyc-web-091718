93c8181fe82dc11c8c09626fe5160fa3602f2693
Eric Klimowich <you@example.com>
2018-09-02 23:57:27 -0400

    Optimus Prime

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(integer)
+  if integer < 2
+    return false
+  end
+  range_array = (2...integer).to_a
+  range_array.each do |int|
+    if integer % int == 0
+      return false
+    end
+  end
+  return true
+end
